a554db37c6c8b00c8dc775732ea5da35691dadea
AuroreAile <127793331+AuroreAile@users.noreply.github.com>
2023-10-17 22:53:28 +0200

    modifiction dé, hold + lancer + changemetn joueur

--- script.js
+++ script.js
@@ -1,73 +1,65 @@
 //console.log ("Mon fichier est lié")
 
-// Variables
+// Variables bouton
 const rollDices = document.getElementById('rollDices')
-const dice = document.getElementById('dice')
+let dice = document.getElementById('dice')
 let scoreCurrent1 = Number(document.getElementById('scoreCurrent1').textContent) 
 let score1 = Number(document.getElementById('score1').textContent)
 let scoreCurrent2 = Number(document.getElementById('scoreCurrent2').textContent)
 let score2 = Number(document.getElementById('score2').textContent)
-const hold1 = document.getElementById('hold1')
-const hold2 = document.getElementById('hold2')
+const hold = document.getElementById('hold')
 const newGame = document.getElementById('newGame')
 const cercle1 = document.getElementById('cercle1')
 const cercle2 = document.getElementById('cercle2')
-let activePlayer = document.getElementById('player1')
-
+const gradient = document.getElementById('gradient')
+const randomDice = Math.floor(Math.random() * 6) + 1
+let activePlayer = true
 
 //bouton "Lancer les dés"
 rollDices.addEventListener('click', () => {
     //afficher le dé
     dice.style.display = 'block'
 
-    //chiffre aléatoire entre 1 et 6 + affichage de l'image correspondante
-        const randomDice = Math.floor(Math.random() * 6) + 1
-        dice.src = "images/dice" + randomDice + ".png"
-    
-    function player1() {
-        //apparaître et disparaître les cercles de couleur qui indique le joueur en cours
-        cercle1.style.display = 'block'
-        cercle2.style.display = 'none'
-        // si chiffre aléatoire différent de 1, ajouter le chiffre aléatoire au score initial et l'afficher sinon afficher 0
-        if (randomDice !== 1) {
-            scoreCurrent1 = scoreCurrent1 + randomDice
-            scoreCurrent1 = document.getElementById('scoreCurrent1').textContent = scoreCurrent1
-        } else {
-            scoreCurrent1 = 0
-            scoreCurrent1 = document.getElementById('scoreCurrent1').textContent = 
-            scoreCurrent1
-            cercle1.style.display = 'none'
-            cercle2.style.display = 'block'
-            return player2()                                           
-        }
-        return
-       }   
-    player1()
-
-   function player2 () {
-        //  /si chiffre aléatoire différent de 1, ajouter le chiffre aléatoire au score initial et l'afficher sinon afficher 0
-        if (randomDice !== 1) {
-            scoreCurrent2 = scoreCurrent2 + randomDice
-            scoreCurrent2 = document.getElementById('scoreCurrent2').textContent = scoreCurrent2
-        } else {
-           return player1()                                                
-      }
-        return
-   }
-   //
-   player2()                                                                  
+    //suite chiffre aléatoire entre 1 et 6 : affichage de l'image correspondante
+    let randomDices = Math.floor(Math.random() * 6) + 1
+    dice.src = "images/dice" + randomDices + ".png"
+                                                                     
+    //si joueur 1 actif appel de la fonction joueur 1 sinon appel de la fonction joueur 2                                                  
+    if (activePlayer) {
+        player1()
+    } else {
+        player2()
+    }                                                                 
 })
 
 
 //modifier le score final au clic sur "Garder le score"
-hold1.addEventListener('click', () => {
+hold.addEventListener('click', () => {
+    //si joueur 1 ectif et si joueur 2 actif
+    if (activePlayer) {
         score1 = scoreCurrent1 + score1
         score1 = document.getElementById('score1').textContent = score1
-})
+    } else {
+        score2 = scoreCurrent2 + score2
+        score2 = document.getElementById('score2').textContent = score2
+    }
+
+    //changer de joueur actif
+    activePlayer = !activePlayer
+
+    //si joueur 1 actif appel de la fonction joueur 1 sinon appel de la fonction joueur 2
+        if (activePlayer) {
+            player2()
+        } else {
+            player1()
+        }                        
+    })
 
 
 // Remise à 0 des paramètres au clic sur "Nouvelle partie"
 newGame.addEventListener('click', () => {
+    cercle1.style.display = 'block'
+    cercle2.style.display = 'none'
     //joueur 1
     scoreCurrent1 = 0
     scoreCurrent1 = document.getElementById('scoreCurrent1').textContent = scoreCurrent1
@@ -83,3 +75,40 @@ newGame.addEventListener('click', () => {
     
 })
 
+//fonction pour le joueur 1
+function player1() {
+    //apparaître et disparaître les cercles de couleur qui indique le joueur en cours
+     cercle1.style.display = 'block'
+     cercle2.style.display = 'none'        
+     // si chiffre aléatoire différent de 1, ajouter le chiffre aléatoire au score initial et l'afficher sinon afficher 0 + passer au joueur 2
+     if (randomDice !== 1) {
+         scoreCurrent1 = scoreCurrent1 + randomDice
+         scoreCurrent1 = document.getElementById('scoreCurrent1').textContent = scoreCurrent1
+     } else {
+         scoreCurrent1 = 0
+         scoreCurrent1 = document.getElementById('scoreCurrent1').textContent = scoreCurrent1
+         cercle1.style.display = 'none'
+         cercle2.style.display = 'block'
+         gradient.style.background = 'linear-gradient(to left, rgb(171, 219, 253) 50%, rgb(180, 224, 254) 50%)'
+         activePlayer = false
+        }
+}  
+
+ //fonction pour le joueur 2
+    function player2 () { 
+       //apparaître et disparaître les cercles de couleur qui indique lejoueur en cours
+     cercle1.style.display = 'none'
+     cercle2.style.display = 'block'
+     //si chiffre aléatoire différent de 1, ajouter le chiffre aléatoire au score initial et l'afficher sinon afficher 0 + passer au joueur 1
+     if (randomDice !== 1) {
+        scoreCurrent2 = scoreCurrent2 + randomDice
+        scoreCurrent2 = document.getElementById('scoreCurrent2').textContent = scoreCurrent2
+     } else {
+        scoreCurrent2 = 0            
+        scoreCurrent2 = document.getElementById('scoreCurrent2').         textContent = scoreCurrent2
+        cercle1.style.display = 'block'
+        cercle2.style.display = 'none'
+        gradient.style.background = 'linear-gradient(to right, rgb(171,219, 253) 50%, rgb(180, 224, 254) 50%)'
+        activePlayer = true
+        }  
+}
